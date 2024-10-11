9016f7b2562a9683237f6b891a12d60d6c8ca743
ge93nil <v.dahmen@tum.de>
2024-10-11 16:50:09 +0200

    tidy setup.py and rename folder

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'runSimplification=osmnetfusion.runSimplification:main',  # Adjust as necessary
+            'runSimplification=osmnetfusion.runSimplification:main',
         ],
     },
     author='Victoria Dahmen',
